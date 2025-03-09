c7f64c32b1a76e8f23c1e4d683daa841ac65c2ae
Gheason <mgff642@gmail.com>
2025-03-09 23:21:23 +0300

    KS-1-4 task Logik OnlineStore

--- src/main/kotlin/Lesson1_task_1/Lesson1_variables.kt
+++ src/main/kotlin/Lesson1_task_1/Lesson1_variables.kt
@@ -1,6 +1,6 @@
 package org.example.Lesson1_task_1
 
 fun main(){
-    var numOrders: Int
+    var numOrders: Int = 75
     var thanksMessage: String = "Благодарим за покупку в нашем онлайн магазине."
 }
